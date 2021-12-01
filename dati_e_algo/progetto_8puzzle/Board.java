abc840acc63f0d141fb11856ab8c0fcac1a0ab11
giulpig <andrea.codutti20@gmail.com>
2021-12-01 20:21:37 +0100

    tested and working code, hashing with strings (no spaces), no compression

--- dati_e_algo/progetto_8puzzle/Board.java
+++ dati_e_algo/progetto_8puzzle/Board.java
@@ -1,74 +1,42 @@
 public class Board{
 
-    public static class Board16 extends Board{
-        public long ctable = 0;
-        byte lato;
+    //public static int[] npowers;
 
-        public Board16(){     //empty, for stealing(?)
-            ;
-        }
-
-        public Board16(long o){     //empty, for stealing(?)
-            ctable = o;
-        }
-
-        public Board16(short[][] tiles){        //for copying
-            lato = (byte)tiles.length;
-            byte counter = 0;
-            for(int i=tiles.length-1; i>=0; i--){           //copio
-                for(int j=tiles.length-1; j>=0; j--){
-                    ctable |= (long)tiles[i][j] << counter;
-                    counter += 4;
-                }
-            }
-        }
-
-        @Override
-        public void calcManhattan(){
-            int ret = 0;
-            long mask = 31; //1111b            
-
-            for(byte i=0; i<lato*lato; i++){                     //calcolo distanza e trovo buco
-                long el = ctable | (mask<<=1);
-                if(el != 0)
-                    ret += Math.abs(i/lato-(el-1)/table.length) + Math.abs(i%lato-(el-1)%table.length);
-                else{
-                    bucox = (short)(i/lato);
-                    bucoy = (short)(i%lato);
-                }
-            }
-
-            dist = ret;
-        }
-
-    }
-
-
-
-
-
-
-
-
-
-    public short[][] table = null;
-    //public int buco = -1;
-    public short bucox = -1;
-    public short bucoy = -1;
-    //public static int lStringa = 0;
+    public int[][] table = null;
+    public int bucox = -1;
+    public int bucoy = -1;
     public int dist = -1;
+
+
+    //public int hash = 2999;
 
     public Board(){     //empty, for stealing(?)
         ;
-    }    
+    }
 
-    public Board(short[][] tiles){        //for copying
-        //table = Arrays.stream(tiles).map(short[]::clone).toArray(short[][]::new);
+    public Board(int[][] tiles, boolean first){        //for first entry (calc hash)
         
-        
-        table = new short[tiles.length][tiles.length];
+        table = new int[tiles.length][tiles.length];
+
+        //int ntemp = 1;
+        //int ntiles = tiles.length * tiles.length;
+
+        //npowers = new int[ntiles];
+        //int count = 0;
 
         for(int i=0; i<tiles.length; i++){           //copio, ***calcolo hash
+            for(int j=0; j<tiles.length; j++){
+                table[i][j] = tiles[i][j];
+                //npowers[count++] = ntemp;
+                //hash += table[i][j]*(ntemp*=ntiles);
+            }
+        }
+    }
+
+    public Board(int[][] tiles){        //for copying
+        table = new int[tiles.length][tiles.length];
+
+        for(int i=0; i<tiles.length; i++){
             for(int j=0; j<tiles.length; j++){
                 table[i][j] = tiles[i][j];
             }
@@ -77,20 +45,6 @@ public class Board{
 
 
     public final String toString(){
-
-        if(table == null){
-            long ctable = ((Board16)(this)).ctable;
-            long mask = 31; //1111b
-
-            StringBuffer ret = new StringBuffer();
-
-            for(byte i=0; i<((Board16)(this)).lato*((Board16)(this)).lato-1; i++){
-                ret.append((ctable | (mask<<=1)) + " ");
-            }
-            ret.append(ctable | (mask<<=1));
-
-            return ret.toString();
-        }
         
         StringBuffer ret = new StringBuffer();
         for(int i=0; i<table.length; i++){
@@ -104,7 +58,20 @@ public class Board{
         return ret.toString();
     }
 
-    public int manhatan(){
+
+    public final String toStringNoSpaces(){
+        
+        StringBuffer ret = new StringBuffer();
+        for(int i=0; i<table.length; i++){
+            for(int j=0; j<table.length; j++){
+                ret.append(table[i][j]);
+            }
+        }
+        return ret.toString();
+    }
+
+
+    public int manhattan(){
         return dist;
     }
 
@@ -112,8 +79,8 @@ public class Board{
         
         int ret = 0;
 
-        for(short i=0; i<table.length; i++){                     //calcolo distanza e trovo buco
-            for(short j=0; j<table.length; j++){
+        for(int i=0; i<table.length; i++){                     //calcolo distanza e trovo buco
+            for(int j=0; j<table.length; j++){
                 if(table[i][j] != 0)
                     ret += Math.abs(i-(table[i][j]-1)/table.length) + Math.abs(j-(table[i][j]-1)%table.length);
                 else{
@@ -130,9 +97,9 @@ public class Board{
     //////////////////////////////MAYBE COMPRESS??
     public void compress(){
         
-        for(short i=0; i<table.length; i++){
-            for(short j=0; j<table.length; j++){
-                ;
+        for(int i=0; i<table.length; i++){
+            for(int j=0; j<table.length; j++){
+                
             }
         }
 
@@ -142,18 +109,12 @@ public class Board{
 
     @Override
     public int hashCode(){          ///////////////////////////WORK IN PROGESS
-        if(table == null)
-            return Long.hashCode(((Board16)(this)).ctable);
-
-        return toString().hashCode();
+        //return hash;
+        return toStringNoSpaces().hashCode();
     }
 
     @Override
     public boolean equals(Object o){
-
-        if(table == null){
-            return ((Board16)(this)).ctable == ((Board16)(o)).ctable;
-        }
 
         if(dist != ((Board)(o)).dist || bucox != ((Board)(o)).bucox || bucoy != ((Board)(o)).bucoy){
             //System.out.println("Non Conflitto");
--- dati_e_algo/progetto_8puzzle/Solver.java
+++ dati_e_algo/progetto_8puzzle/Solver.java
@@ -7,7 +7,9 @@ import java.io.*;       //Input/Output
 
 public class Solver{
 
-    /////////////////////////////////////////////
+    static public int counter = 0;    //to remove
+
+    ////////////////////////////////////////////
     //Tree class
     ////////////////////////////////////////////
 
@@ -40,7 +42,7 @@ public class Solver{
                     System.out.print("\r" + (current.moves + current.board.dist) + "   ");
                     lastLevel = (short)(current.moves + current.board.dist);
                 }
-                //System.out.print("\r" + nodes);
+                //System.out.print("\r" + nodes + " ");
                 //System.out.print("\r" + current.sons.length);
                 //System.out.print("\r" + current.board.toString());
                 
@@ -99,13 +101,16 @@ public class Solver{
                     else{
                         ret.board.dist = board.dist + 1;
                     }
+                    
+                    //------aggiorno hash modificando tile sopra il buco------
+                    //ret.board.hash = board.hash + ret.board.table[board.bucox-1][board.bucoy] * (Board.npowers[board.table.length*(board.bucox)+board.bucoy] - Board.npowers[board.table.length*(board.bucox-1)+board.bucoy]);
                 
                     //swap
                     ret.board.table[board.bucox][board.bucoy] = ret.board.table[board.bucox-1][board.bucoy];
                     ret.board.table[board.bucox-1][board.bucoy] = 0;
 
                     //update posizione buco, sale
-                    ret.board.bucox = (short)(board.bucox-1);
+                    ret.board.bucox = board.bucox-1;
                     ret.board.bucoy = board.bucoy;
 
                     checkCollision(ret);
@@ -125,13 +130,16 @@ public class Solver{
                     else{
                         ret.board.dist = board.dist + 1;
                     }
+                    
+                    //------aggiorno hash modificando tile sopra il buco------
+                    //ret.board.hash = board.hash + ret.board.table[board.bucox][board.bucoy-1] * (Board.npowers[board.table.length*(board.bucox)+board.bucoy] - Board.npowers[board.table.length*(board.bucox)+board.bucoy-1]);
 
                     //swap
                     ret.board.table[board.bucox][board.bucoy] = ret.board.table[board.bucox][board.bucoy-1];
                     ret.board.table[board.bucox][board.bucoy-1] = 0;
 
                     //update buco, va a sx
-                    ret.board.bucoy = (short)(board.bucoy-1);
+                    ret.board.bucoy = board.bucoy-1;
                     ret.board.bucox = board.bucox;
 
                     checkCollision(ret);
@@ -151,13 +159,16 @@ public class Solver{
                     else{
                         ret.board.dist = board.dist + 1;
                     }
+
+                    //------aggiorno hash modificando tile sopra il buco------
+                    //ret.board.hash = board.hash + ret.board.table[board.bucox][board.bucoy+1] * (Board.npowers[board.table.length*(board.bucox)+board.bucoy] - Board.npowers[board.table.length*(board.bucox)+board.bucoy+1]);
                     
                     //swap
                     ret.board.table[board.bucox][board.bucoy] = ret.board.table[board.bucox][board.bucoy+1];
                     ret.board.table[board.bucox][board.bucoy+1] = 0;
 
                     //update buco, va a dx
-                    ret.board.bucoy = (short)(board.bucoy+1);
+                    ret.board.bucoy = board.bucoy+1;
                     ret.board.bucox = board.bucox;
 
                     checkCollision(ret);
@@ -178,12 +189,17 @@ public class Solver{
                         ret.board.dist = board.dist + 1;
                     }
                     
+
+                    //------aggiorno hash modificando tile sopra il buco------
+                    //ret.board.hash = board.hash + ret.board.table[board.bucox+1][board.bucoy] * (Board.npowers[board.table.length*(board.bucox)+board.bucoy] - Board.npowers[board.table.length*(board.bucox+1)+board.bucoy]);
+
+
                     //swap
                     ret.board.table[board.bucox][board.bucoy] = ret.board.table[board.bucox+1][board.bucoy];
                     ret.board.table[board.bucox+1][board.bucoy] = 0;
 
                     //update buco, va in basso
-                    ret.board.bucox = (short)(board.bucox+1);
+                    ret.board.bucox = board.bucox+1;
                     ret.board.bucoy = board.bucoy;
 
                     checkCollision(ret);
@@ -227,201 +243,10 @@ public class Solver{
                 return board.dist+moves == (((TreeNode)(o)).board.dist+((TreeNode)(o)).moves);
             }
         }
-
-
-
-        /////////////////////////////////////////////
-        //TreeNode16 class
-        /////////////////////////////////////////////
-
-        public static class TreeNode16 extends TreeNode{
-
-            public static HashMap<Board, Tree.TreeNode16> activeBoards = new HashMap<Board, Tree.TreeNode16>(2999, 0.5f);
-            
-            public static long mask = 31;   //1111b
-
-            public short moves;       //mosse per arrivare qua === depth dell'albero
-            public byte lastMove;    //ultima mossa fatta: 0->radice 1->su 2->sx 3->dx 4->giu
-            public Board.Board16 board;
-            //public boolean alive = true;
-
-            public TreeNode16 father;
-
-            public TreeNode16(Board.Board16 b, short m, TreeNode16 f, byte l){
-                board = b;
-                moves = m;
-                father = f;
-                lastMove = l;
-
-                Tree.nodes++;       //toremove
-                //System.out.print("\r" + nodes + " " + activeBoards.size());
-            }
-
-
-            public void calcSons(){
-
-                Board.Board16 newBoard;
-                TreeNode16 ret;
-
-                if(lastMove!=4 && board.bucox > 0){          //swap in alto
-                
-                    newBoard = new Board.Board16(board.ctable);
-
-                    ret = new TreeNode16(newBoard, (short)(moves+1), this, (byte)1);
-
-                    //OKKKIOIOIOIOIOI, il buco e' l'indice da destra
-                    byte buco = (byte)(board.lato - (board.bucox*board.lato + board.bucoy)*4);
-
-                    //aggiorno dist: real-expected
-                    if(board.bucox-1 < ((board.ctable|(mask<<(buco-(board.lato*4))))-1)/ret.board.table.length){
-                        ret.board.dist = board.dist - 1;
-                    }
-                    else{
-                        ret.board.dist = board.dist + 1;
-                    }
-                
-                    //swap
-                    ret.board.table[board.bucox][board.bucoy] = ret.board.table[board.bucox-1][board.bucoy];
-                    ret.board.table[board.bucox-1][board.bucoy] = 0;
-
-                    ret.board.ctable = 0;
-
-                    //update posizione buco, sale
-                    ret.board.bucox = (short)(board.bucox-1);
-                    ret.board.bucoy = board.bucoy;
-
-                    checkCollision(ret);
-                }
-
-
-                if(lastMove!=3 && board.bucoy > 0){          //swap a sx
-                
-                    newBoard = new Board.Board16(board.table);
-
-                    ret = new TreeNode16(newBoard, (short)(moves+1), this, (byte)2);
-
-                    //aggiorno dist: real-expected, casella swappata va a dx
-                    if(board.bucoy-1 < (ret.board.table[board.bucox][board.bucoy-1]-1)%ret.board.table.length){
-                        ret.board.dist = board.dist - 1;
-                    }
-                    else{
-                        ret.board.dist = board.dist + 1;
-                    }
-
-                    //swap
-                    ret.board.table[board.bucox][board.bucoy] = ret.board.table[board.bucox][board.bucoy-1];
-                    ret.board.table[board.bucox][board.bucoy-1] = 0;
-
-                    //update buco, va a sx
-                    ret.board.bucoy = (short)(board.bucoy-1);
-                    ret.board.bucox = board.bucox;
-
-                    checkCollision(ret);
-                }
-
-
-                if(lastMove!=2 && board.bucoy < board.table.length-1){       //swap a dx
-                
-                    newBoard = new Board.Board16(board.table);
-
-                    ret = new TreeNode16(newBoard, (short)(moves+1), this, (byte)3);
-
-                    //aggiorno dist: real-expected, casella swappata va a sx
-                    if(board.bucoy+1 > (ret.board.table[board.bucox][board.bucoy+1]-1)%ret.board.table.length){
-                        ret.board.dist = board.dist - 1;
-                    }
-                    else{
-                        ret.board.dist = board.dist + 1;
-                    }
-            
-                    //swap
-                    ret.board.table[board.bucox][board.bucoy] = ret.board.table[board.bucox][board.bucoy+1];
-                    ret.board.table[board.bucox][board.bucoy+1] = 0;
-
-                    //update buco, va a dx
-                    ret.board.bucoy = (short)(board.bucoy+1);
-                    ret.board.bucox = board.bucox;
-
-                    checkCollision(ret);
-                }
-
-
-                if(lastMove!=1 && board.bucox < board.table.length-1){      //swap in basso
-                
-                    newBoard = new Board.Board16(board.table);
-
-                    ret = new TreeNode16(newBoard, (short)(moves+1), this, (byte)4);
-
-                    //aggiorno dist: real-expected
-                    if(board.bucox+1 > (ret.board.table[board.bucox+1][board.bucoy]-1)/ret.board.table.length){
-                        ret.board.dist = board.dist - 1;
-                    }
-                    else{
-                        ret.board.dist = board.dist + 1;
-                    }
-                
-                    //swap
-                    ret.board.table[board.bucox][board.bucoy] = ret.board.table[board.bucox+1][board.bucoy];
-                    ret.board.table[board.bucox+1][board.bucoy] = 0;
-
-                    //update buco, va in basso
-                    ret.board.bucox = (short)(board.bucox+1);
-                    ret.board.bucoy = board.bucoy;
-
-                    checkCollision(ret);
-                }
-
-            }
-
-
-            /*public static void checkCollision(TreeNode ret){
-                //se gia' esiste, agisco su activeBoards
-                TreeNode collisioner = activeBoards.get(ret.board);
-                if(collisioner != null){
-
-                    if(collisioner.board.dist+collisioner.moves <= ret.board.dist+ret.moves){
-                        ret = null;
-                    }
-                    else{
-                        activeBoards.remove(collisioner.board);
-                        pQueue.remove(collisioner);
-                        collisioner = null;
-
-                        pQueue.add(ret);
-                        activeBoards.put(ret.board, ret);
-                    }
-                }
-                else{
-                    pQueue.add(ret);
-                    activeBoards.put(ret.board, ret);
-                }
-
-            }
-
-
-            public int compareTo(TreeNode o){
-
-                int diff = board.dist+moves - (o.board.dist+o.moves);
-
-                if(diff>0)
-                    return 1;
-                
-                if(diff<0)
-                    return -1;
-                
-                return 0;
-            }
-
-            @Override
-            public boolean equals(Object o){
-                return board.dist+moves == (((TreeNode)(o)).board.dist+((TreeNode)(o)).moves);
-            }
-            */
-
-        }
-
-
     }
+
+
+
 
     /////////////////////////////////////////////
     //IO operations
@@ -450,26 +275,26 @@ public class Solver{
             return st.nextToken();
         }
 
-        short nextShort() { return Short.parseShort(next()); }
+        int nextInt() { return Integer.parseInt(next()); }
     }
 
     public static Board readInput(String filename) throws FileNotFoundException{
         
         FastReader sc = new FastReader(filename);
 
-        short lato = sc.nextShort();
+        int lato = sc.nextInt();
 
-        short[][] tabella = new short[lato][lato];
+        int[][] tabella = new int[lato][lato];
 
-        short n = (short)(lato*lato);
-        short counter = 0;
+        int n = lato*lato;
+        int counter = 0;
 
         while(n-- > 0){
-            tabella[counter/lato][counter%lato] = sc.nextShort();
+            tabella[counter/lato][counter%lato] = sc.nextInt();
             counter++;
         }
 
-        return new Board(tabella);
+        return new Board(tabella, true);
 
     }
 
@@ -509,12 +334,7 @@ public class Solver{
 
         Tree.TreeNode radice;
 
-        /*if(tab.table.length <= 4){
-            radice = new Tree.TreeNode16(tab, (short)(0), null, (byte)0);
-        }
-        else{*/
-            radice = new Tree.TreeNode(tab, (short)(0), null, (byte)0);
-        //}
+        radice = new Tree.TreeNode(tab, (short)(0), null, (byte)0);
 
         Tree albero = new Tree(radice);
 
